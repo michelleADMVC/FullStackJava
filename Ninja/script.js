d6d67f7fca0857bb7f699452158e4ea22824d482
MichelleADMVC <michelle.admvc@gmail.com>
2020-02-10 12:07:41 -0600

    ninja desapareciendo terminado

--- Ninja/script.js
+++ Ninja/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $('.ninja_img').click(function(){
-        $(this).hide();
+        $(this).hide("linear");
     });
     $('#restore').click(function(){
-        
+        $('.ninja_img').show("swing");
     });
 });
