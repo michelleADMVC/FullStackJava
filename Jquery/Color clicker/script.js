4ad4271d862b294914ddffc96b6a289e33bd678e
MichelleADMVC <michelle.admvc@gmail.com>
2020-02-11 13:02:02 -0600

    Color clicker

---
+++ Jquery/Color clicker/script.js
@@ -0,0 +1,25 @@
+function random_color() {
+    var rgb = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    color = '#'  //this is what we'll return!
+    for (var i = 0; i < 6; i++) {
+        x = Math.floor((Math.random() * 16))
+        color += rgb[x];
+    }
+    return color;
+}
+$(document).ready(function () {
+    $('#large_box').click(function () {
+        
+        $(this).children().css("background-color","#92073E");
+        
+       
+    })
+    $('.side_box').click(function (event) {
+        event.stopPropagation();
+        $(this).siblings().css("background-color", " #5B1546");
+    })
+    $('.middle_box').click(function (event) {
+        event.stopPropagation();
+        $(this).parent().css("background-color", " #C90035");
+    })
+});
